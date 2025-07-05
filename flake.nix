4b4194360e9bc2c62667e33b6c87483028e0e63a
KarotPwintu <karotpwintu@proton.me>
2025-07-05 15:22:55 +0200

    test

--- flake.nix
+++ flake.nix
@@ -21,75 +21,36 @@
 {
   description = "Black Magic for witches";
 
+  outputs = inputs:
+    inputs.snowfall-lib.mkFlake {
+      inherit inputs;
+      src = ./.;
+      snowfall = {
+        root = ./nix;
+        namespace = "blackmagic";
+        meta = {
+          name = "witchcraft";
+          title = "Witchcraft";
+        };
+      };
+    };
+
   inputs = {
+
+    #=Core=#
     lix-module = {
       url = "https://git.lix.systems/lix-project/nixos-module/archive/2.93.0.tar.gz";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     nixpkgs = {
-      type = "github";
-      owner = "nixos";
-      repo = "nixpkgs";
-      ref = "nixos-unstable";
-    };
-    flake-parts = {
-      type = "github";
-      owner = "hercules-ci";
-      repo = "flake-parts";
+      url = "github:nixos/nixpkgs/nixos-24.05";
     };
     home = {
-      type = "github";
-      owner = "nix-community";
-      repo = "home-manager";
+      url = "github:nix-community/home-manager";
     };
-    hyprland = {
-      type = "github";
-      owner = "hyprwm";
-      repo = "Hyprland";
-    };
-    hyprland-plugins = {
-      type = "github";
-      owner = "hyprwm";
-      repo = "hyprland-plugins";
-      inputs.hyprland.follows = "hyprland";
-    };
-    pyprland = {
-      type = "github";
-      owner = "hyprland-community";
-      repo = "pyprland";
-    };
-    swww = {
-      type = "github";
-      owner = "LGFae";
-      repo = "swww";
-    };
-    ags = {
-      type = "github";
-      owner = "aylur";
-      repo = "ags";
-    };
-    catppuccin = {
-      type = "github";
-      owner = "catppuccin";
-      repo = "nix";
-    };
-    spicetify = {
-      type = "github";
-      owner = "Gerg-L";
-      repo = "spicetify-nix";
-    };
-    apple-fonts = {
-      url = "github:Lyndeno/apple-fonts.nix";
+    snowfall-lib = {
+      url = "github:snowfallorg/lib";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
   };
-
-  outputs = { flake-parts, ... }@inputs: 
-    flake-parts.lib.mkFlake { inherit inputs; } {
-      systems = [
-        "x86_64-linux"
-      ];
-      imports = [
-        ./modules
-      ];
-    };
 }
