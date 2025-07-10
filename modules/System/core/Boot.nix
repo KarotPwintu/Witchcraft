7d91d96f756c652888f23bd6de267dfa58936a43
KarotPwintu <karotpwintu@proton.me>
2025-07-10 16:57:36 +0000

    Update Boot.nix

--- modules/System/core/Boot.nix
+++ modules/System/core/Boot.nix
@@ -22,7 +22,7 @@
   };
   #environment.systemPackages = [ pkgs.sbctl ];
   boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
+  #boot.loader.efi.canTouchEfiVariables = true;
   boot = {
     kernelPackages = pkgs.linuxKernel.packages.linux_lqx;
     plymouth = {
